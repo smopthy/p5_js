ad89eee05a6e41c2e97ca6627e79b63f4ff4c052
smopthy <32154707+smopthy@users.noreply.github.com>
2018-06-02 14:52:17 +0800

    mosaic rect
    based on p5  js , 
    ideal inspired by Daniel Shiffman
    #array # p5 js

---
+++ mosaic/sketch.js
@@ -0,0 +1,38 @@
+
+let cols = 10 ; 
+
+let raws = 10 ;
+
+let colors = [] ; 
+
+function setup () {
+	createCanvas(310,310); 
+
+} 
+
+
+function draw(){
+	background(150);
+	mode
+
+	for(let i=0 ; i < cols ; i++ ){
+		colors[i] = [] ; 
+		for( let j = 0 ; j < raws ; j++) {
+
+			colors[i][j] = radom(255);
+		} 
+	}
+
+	for( let i=0 ; i < cols ; i++) {
+		for ( let j = 0 ; j < raws ; j++ ){
+			x = i * 30 ;
+			y = j * 30 ;
+
+			fill(colors[i][j]);
+			rect(x , y , 30 ,30 )
+		}
+
+
+
+	}
+}
